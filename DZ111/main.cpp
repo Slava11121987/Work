b4442b7ada606fa52c778e6fc8e57537a0b1978d
Slava <syavka201@gmail.com>
2021-02-02 16:36:34 +0300

    DZ1

---
+++ DZ111/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+   int a=5;
+   int c=5;
+    printf("a=%d,c=%d\n",a,c);
+    printf("a=a+b-2\n");
+    printf("c=c+1,d=c-a+d\n");
+    printf("a=a*c,c=c-1\n");
+    printf("a=a/10,c=c/2,b=b-1\n");
+    printf("d=d*(c+b+a)");
+    return 0;
+}
