2ee9b6497a69ddda5e0fb1bc187aaac35c38374d
Slava <syavka201@gmail.com>
2021-06-11 13:36:25 +0300

    proba

--- DZ2/main.cpp
+++ DZ2/main.cpp
@@ -9,5 +9,4 @@ int main()
     double result = ((2 * cos( x - (M_PI / 6)) + sqrt(2)) * exp(3*x))
                        / (1/(2*log(x)) + pow(sin(pow(x,2)), 2));
     printf( "%lf" , result);
-      return 0;
 }
